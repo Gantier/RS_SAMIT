9cc0206d54d65df38b63016294d37ad890907406
maltssbm <42448116+maltssbm@users.noreply.github.com>
2019-03-28 20:39:21 -0400

    Add files via upload
    CC Filters, still broken

--- cc_graduate.php
+++ cc_graduate.php
@@ -1,36 +1,94 @@
 <?php
-    require "header.php";
+require "header.php";
 
-    require "includes/dbh.inc.php";
+require "includes/dbh.inc.php";
 
-    $sql = "SELECT c.courseName, c.courseNumber, c.courseSubject, c.courseSubject, c.courseCredits, c.courseAttribute 
-            FROM registration_system.course_graduate g, registration_system.course c 
-            WHERE g.courseGraduateName LIKE c.courseName
-            ORDER BY c.courseSubject, c.courseNumber";
-    $statement = mysqli_stmt_init($conn);
+if (isset($_POST['cc-filter-submit']))
+{
+    //Keyword
+    $keyword = $_POST['keyword'];
 
-    if (!mysqli_stmt_prepare($statement, $sql))
+    //Range
+    if (!($_POST['range-min']))
     {
-        header("Location: ../cc_graduate.php?error=sqlError");
-        exit();
+        $min = 0;
     }
     else
     {
-        mysqli_stmt_execute($statement);
-        $result = mysqli_stmt_get_result($statement);
-        if ($row = mysqli_fetch_assoc($result))
-        {
-            $class = "cc";
-            $caption = "Graduate Courses";
-            require "includes/sql2html.inc.php";
-        }
-        else
-        {
-            header("Location: ../index.php?error=noUser");
-            exit();
-        }
+        $min = $_POST['range-min'];
+    }
+    if (!($_POST['range-max']))
+    {
+        $max = PHP_INT_MAX;
+    }
+    else
+    {
+        $max = $_POST['range-max'];
     }
 
-    require "includes/update_cc_form.inc.php";
+    //Subject
 
-    require "footer.php";
+    // WRITE THE SQL STATEMENT SEQUENTIALLY
+    
+    $subject = $_POST['subject-dropdown'];
+    $attribute = $_POST['attribute-dropdown'];
+
+    if($subject == "HiddenOption" && $attribute == "HiddenOption") //IF NO DROPDOWN IS NOT SELECTED
+    {
+        $sql = "SELECT c.courseName, CAST(c.courseNumber AS UNSIGNED) AS courseNumber, c.courseSubject, c.courseCredits, c.courseAttribute 
+            FROM registration_system.course_graduate g, registration_system.course c 
+            WHERE (g.courseGraduateName LIKE c.courseName) AND 
+            (c.courseName LIKE '%" . $keyword . "%' OR 
+            c.courseSubject LIKE '%" . $keyword . "%' OR
+            c.courseAttribute LIKE '%" . $keyword . "%') AND
+            (c.courseNumber >= $min AND c.courseNumber <= $max)
+            ORDER BY c.courseSubject, c.courseNumber";
+
+    }
+    else //IF DROPDOWN IS SELECTED
+    {
+        $sql = "SELECT c.courseName, CAST(c.courseNumber AS UNSIGNED) AS courseNumber, c.courseSubject, c.courseCredits, c.courseAttribute 
+            FROM registration_system.course_graduate g, registration_system.course c 
+            WHERE (g.courseGraduateName LIKE c.courseName) AND 
+            (c.courseName LIKE '%" . $keyword . "%' OR 
+            c.courseSubject LIKE '%" . $keyword . "%' OR
+            c.courseAttribute LIKE '%" . $keyword . "%') AND
+            (c.courseNumber >= $min AND c.courseNumber <= $max) AND
+            c.courseSubject LIKE '" . $subject . "'
+            ORDER BY c.courseSubject, c.courseNumber";
+    }
+}
+else
+{
+    $sql = "SELECT c.courseName, c.courseNumber, c.courseSubject, c.courseCredits, c.courseAttribute 
+            FROM registration_system.course_graduate g, registration_system.course c 
+            WHERE g.courseGraduateName LIKE c.courseName
+            ORDER BY c.courseSubject, c.courseNumber";
+}
+$statement = mysqli_stmt_init($conn);
+
+if (!mysqli_stmt_prepare($statement, $sql))
+{
+    header("Location: ../cc_graduate.php?error=sqlError");
+    exit();
+}
+else
+{
+    mysqli_stmt_execute($statement);
+    $result = mysqli_stmt_get_result($statement);
+    if ($row = mysqli_fetch_assoc($result))
+    {
+        $class = "cc";
+        $caption = "Graduate Courses";
+        require "includes/sql2html.inc.php";
+    }
+    else
+    {
+        header("Location: cc_graduate.php");
+        exit();
+    }
+}
+
+require "includes/update_cc_form.inc.php";
+
+require "footer.php";
--- cc_undergraduate.php
+++ cc_undergraduate.php
@@ -1,36 +1,49 @@
 <?php
-    require "header.php";
+require "header.php";
 
-    require "includes/dbh.inc.php";
+require "includes/dbh.inc.php";
 
-    $sql = "SELECT c.courseName, c.courseNumber, c.courseSubject, c.courseSubject, c.courseCredits, c.courseAttribute 
+if (isset($_POST['cc-filter-submit']))
+{
+    $keyword = $_POST['keyword'];
+    $sql = "SELECT c.courseName, c.courseNumber, c.courseSubject, c.courseCredits, c.courseAttribute 
+            FROM registration_system.course_undergraduate g, registration_system.course c 
+            WHERE (g.courseUndergraduateName LIKE c.courseName) AND (c.courseName LIKE '%" . $keyword . "%' OR 
+            c.courseSubject LIKE '%" . $keyword . "%' OR
+            c.courseAttribute LIKE '%" . $keyword . "%')
+            ORDER BY c.courseSubject, c.courseNumber";
+}
+else
+{
+    $sql = "SELECT c.courseName, c.courseNumber, c.courseSubject, c.courseCredits, c.courseAttribute 
             FROM registration_system.course_undergraduate g, registration_system.course c 
             WHERE g.courseUndergraduateName LIKE c.courseName
             ORDER BY c.courseSubject, c.courseNumber";
-    $statement = mysqli_stmt_init($conn);
+}
+$statement = mysqli_stmt_init($conn);
 
-    if (!mysqli_stmt_prepare($statement, $sql))
+if (!mysqli_stmt_prepare($statement, $sql))
+{
+    header("Location: ../cc_undergraduate.php?error=sqlError");
+    exit();
+}
+else
+{
+    mysqli_stmt_execute($statement);
+    $result = mysqli_stmt_get_result($statement);
+    if ($row = mysqli_fetch_assoc($result))
     {
-        header("Location: ../cc_undergraduate.php?error=sqlError");
-        exit();
+        $class = "cc";
+        $caption = "Undergraduate Courses";
+        require "includes/sql2html.inc.php";
     }
     else
     {
-        mysqli_stmt_execute($statement);
-        $result = mysqli_stmt_get_result($statement);
-        if ($row = mysqli_fetch_assoc($result))
-        {
-            $class = "cc";
-            $caption = "Undergraduate Courses";
-            require "includes/sql2html.inc.php";
-        }
-        else
-        {
-            header("Location: ../index.php?error=noData");
-            exit();
-        }
+        header("Location: ../index.php?error=noData");
+        exit();
     }
+}
 
-    require "includes/update_cc_form.inc.php";
+require "includes/update_cc_form.inc.php";
 
-    require "footer.php";
+require "footer.php";
