05761696139e5e2a2e08a28edc0e24f1ebc995b3
maltssbm <42448116+maltssbm@users.noreply.github.com>
2019-03-29 20:41:38 -0400

    Add files via upload

--- cc_graduate.php
+++ cc_graduate.php
@@ -3,12 +3,11 @@ require "header.php";
 
 require "includes/dbh.inc.php";
 
-if (isset($_POST['cc-filter-submit']))
+//Old code, currently obsolete but saving just in case
+/*if (isset($_POST['cc-filter-submit']))
 {
-    //Keyword
     $keyword = $_POST['keyword'];
 
-    //Range
     if (!($_POST['range-min']))
     {
         $min = 0;
@@ -26,14 +25,10 @@ if (isset($_POST['cc-filter-submit']))
         $max = $_POST['range-max'];
     }
 
-    //Subject
-
-    // WRITE THE SQL STATEMENT SEQUENTIALLY
-    
     $subject = $_POST['subject-dropdown'];
     $attribute = $_POST['attribute-dropdown'];
 
-    if($subject == "HiddenOption" && $attribute == "HiddenOption") //IF NO DROPDOWN IS NOT SELECTED
+    if ($subject == "HiddenOption" && $attribute == "HiddenOption") //IF NO DROPDOWN IS NOT SELECTED
     {
         $sql = "SELECT c.courseName, CAST(c.courseNumber AS UNSIGNED) AS courseNumber, c.courseSubject, c.courseCredits, c.courseAttribute 
             FROM registration_system.course_graduate g, registration_system.course c 
@@ -60,11 +55,84 @@ if (isset($_POST['cc-filter-submit']))
 }
 else
 {
+    $sql = "SELECT c.courseName, c.courseNumber, c.courseSubject, c.courseCredits, c.courseAttribute
+            FROM registration_system.course_graduate g, registration_system.course c
+            WHERE g.courseGraduateName LIKE c.courseName
+            ORDER BY c.courseSubject, c.courseNumber";
+}*/
+
+//Sequential construction of sql query:
+
+if (isset($_POST['cc-filter-submit']))
+{
+//Step 0: Guaranteed part of sql
+    $sql = "SELECT c.courseName, c.courseNumber, c.courseSubject, c.courseCredits, c.courseAttribute 
+            FROM registration_system.course_graduate g, registration_system.course c 
+            WHERE (g.courseGraduateName LIKE c.courseName)";
+
+//Step 1: Check KEYWORD search
+    $keyword = $_POST['keyword'];
+    if (!(empty($keyword)))
+    {
+        $sql .= " AND (c.courseName LIKE '%" . $keyword . "%') ";
+    }
+
+//Step 2: Check RANGES
+    $min = $_POST['range-min'];
+    $max = $_POST['range-max'];
+
+    if (empty($min))
+    {
+        $min = 0;
+    }
+    if (empty($max))
+    {
+        $max = PHP_INT_MAX;
+    }
+
+    /*Potential optimization. This would save one SQL condition,
+    but add one PHP condition. Not sure how that works
+    out realistically, so this is omitted for now.
+
+    if(both $min and $max are empty)
+    {
+        don't touch the sql
+    }
+    else
+    {
+        add condition to sql
+    }
+    */
+
+    $sql .= " AND (c.courseNumber >= $min AND c.courseNumber <= $max) ";
+
+//Step 3: Check SUBJECT dropdown
+    $subject = $_POST['subject-dropdown'];
+
+    if ($subject != "HiddenOption")
+    {
+        $sql .= " AND c.courseSubject LIKE '" . $subject . "' ";
+    }
+
+//Step 4: Check ATTRIBUTE dropdown
+    $attribute = $_POST['attribute-dropdown'];
+
+    if ($attribute != "HiddenOption")
+    {
+        $sql .= " AND c.courseAttribute LIKE '" . $attribute . "' ";
+    }
+
+//Step Final: Guaranteed closing part of sql
+    $sql = $sql . " ORDER BY c.courseSubject, c.courseNumber";
+}
+else
+{
     $sql = "SELECT c.courseName, c.courseNumber, c.courseSubject, c.courseCredits, c.courseAttribute 
             FROM registration_system.course_graduate g, registration_system.course c 
             WHERE g.courseGraduateName LIKE c.courseName
             ORDER BY c.courseSubject, c.courseNumber";
 }
+
 $statement = mysqli_stmt_init($conn);
 
 if (!mysqli_stmt_prepare($statement, $sql))
--- cc_undergraduate.php
+++ cc_undergraduate.php
@@ -3,7 +3,8 @@ require "header.php";
 
 require "includes/dbh.inc.php";
 
-if (isset($_POST['cc-filter-submit']))
+//Obsolete code:
+/*if (isset($_POST['cc-filter-submit']))
 {
     $keyword = $_POST['keyword'];
     $sql = "SELECT c.courseName, c.courseNumber, c.courseSubject, c.courseCredits, c.courseAttribute 
@@ -12,6 +13,76 @@ if (isset($_POST['cc-filter-submit']))
             c.courseSubject LIKE '%" . $keyword . "%' OR
             c.courseAttribute LIKE '%" . $keyword . "%')
             ORDER BY c.courseSubject, c.courseNumber";
+}
+else
+{
+    $sql = "SELECT c.courseName, c.courseNumber, c.courseSubject, c.courseCredits, c.courseAttribute
+            FROM registration_system.course_undergraduate g, registration_system.course c 
+            WHERE g.courseUndergraduateName LIKE c.courseName
+            ORDER BY c.courseSubject, c.courseNumber";
+}*/
+
+if (isset($_POST['cc-filter-submit']))
+{
+//Step 0: Guaranteed part of sql
+    $sql = "SELECT c.courseName, c.courseNumber, c.courseSubject, c.courseCredits, c.courseAttribute 
+            FROM registration_system.course_undergraduate g, registration_system.course c 
+            WHERE (g.courseUndergraduateName LIKE c.courseName)";
+
+//Step 1: Check KEYWORD search
+    $keyword = $_POST['keyword'];
+    if (!(empty($keyword)))
+    {
+        $sql .= " AND (c.courseName LIKE '%" . $keyword . "%') ";
+    }
+
+//Step 2: Check RANGES
+    $min = $_POST['range-min'];
+    $max = $_POST['range-max'];
+
+    if (empty($min))
+    {
+        $min = 0;
+    }
+    if (empty($max))
+    {
+        $max = PHP_INT_MAX;
+    }
+
+    /*Potential optimization. This would save one SQL condition,
+    but add one PHP condition. Not sure how that works
+    out realistically, so this is omitted for now.
+
+    if(both $min and $max are empty)
+    {
+        don't touch the sql
+    }
+    else
+    {
+        add condition to sql
+    }
+    */
+
+    $sql .= " AND (c.courseNumber >= $min AND c.courseNumber <= $max) ";
+
+//Step 3: Check SUBJECT dropdown
+    $subject = $_POST['subject-dropdown'];
+
+    if ($subject != "HiddenOption")
+    {
+        $sql .= " AND c.courseSubject LIKE '" . $subject . "' ";
+    }
+
+//Step 4: Check ATTRIBUTE dropdown
+    $attribute = $_POST['attribute-dropdown'];
+
+    if ($attribute != "HiddenOption")
+    {
+        $sql .= " AND c.courseAttribute LIKE '" . $attribute . "' ";
+    }
+
+//Step Final: Guaranteed closing part of sql
+    $sql = $sql . " ORDER BY c.courseSubject, c.courseNumber";
 }
 else
 {
@@ -39,7 +110,7 @@ else
     }
     else
     {
-        header("Location: ../index.php?error=noData");
+        header("Location: cc_undergraduate.php");
         exit();
     }
 }
